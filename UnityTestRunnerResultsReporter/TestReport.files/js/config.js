9626da621a1696a404fe5b44a0791ca03ff995fb
Alex Evgrashin <aevgrashin@yandex.ru>
2020-02-16 12:29:51 +0300

    Added extent test reporter

--- UnityTestRunnerResultsReporter/TestReport.files/js/config.js
+++
@@ -1,6 +0,0 @@
-(function () {
-	var testReportApp = angular.module('testReportApp');
-	testReportApp.config(["$provide", function ($provide) {
-		$provide.value('data', JSON.parse(document.getElementById('data').innerHTML));
-	}]);
-})();
