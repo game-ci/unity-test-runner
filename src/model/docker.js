7c1cba9a39d427378c17142b457b1266b272f445
litefeel <litefeel@gmail.com>
2020-03-05 00:51:47 +0800

    Change UNITY_LICENSE_PATH to UNITY_LICENSE_FILE

--- src/model/docker.js
+++ src/model/docker.js
@@ -31,7 +31,7 @@ class Docker {
         --workdir /github/workspace \
         --rm \
         --env UNITY_LICENSE \
-        --env UNITY_LICENSE_PATH \
+        --env UNITY_LICENSE_FILE \
         --env UNITY_EMAIL \
         --env UNITY_PASSWORD \
         --env UNITY_SERIAL \
