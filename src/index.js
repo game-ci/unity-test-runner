2656f4e108a460d0f51dde9646d9eff8c8693c1a
Vladimir Kryukov <erlioniel@gmail.com>
2021-01-17 23:55:27 +0300

    Cherry pick try-catch block to output results if build is failing (#90)

--- src/index.js
+++ src/index.js
@@ -16,22 +16,24 @@ async function action() {
   } = Input.getFromUser();
   const baseImage = ImageTag.createForBase({ version: unityVersion, customImage });
 
-  // Build docker image
-  const actionImage = await Docker.build({ path: actionFolder, dockerfile, baseImage });
+  try {
+    // Build docker image
+    const actionImage = await Docker.build({ path: actionFolder, dockerfile, baseImage });
 
-  // Run docker image
-  await Docker.run(actionImage, {
-    workspace,
-    unityVersion,
-    projectPath,
-    testMode,
-    artifactsPath,
-    useHostNetwork,
-    customParameters,
-  });
-
-  // Set output
-  await Output.setArtifactsPath(artifactsPath);
+    // Run docker image
+    await Docker.run(actionImage, {
+      workspace,
+      unityVersion,
+      projectPath,
+      testMode,
+      artifactsPath,
+      useHostNetwork,
+      customParameters,
+    });
+  } finally {
+    // Set output
+    await Output.setArtifactsPath(artifactsPath);
+  }
 }
 
 action().catch(error => {
