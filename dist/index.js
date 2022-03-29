4762abda488981cefbd4ac075047cbc5fb6884b7
Paul Pacheco <paulpach@gmail.com>
2022-03-29 09:44:40 -0500

    run with bash

--- dist/index.js
+++ dist/index.js
@@ -221,7 +221,7 @@ const Docker = {
         ${githubToken ? '--env USE_EXIT_CODE=false' : '--env USE_EXIT_CODE=true'} \
         ${image} \
         --
-        /entrypoint.sh`;
+        bash /entrypoint.sh`;
             yield (0, exec_1.exec)(command, undefined, { silent });
         });
     },
--- src/model/docker.ts
+++ src/model/docker.ts
@@ -81,7 +81,7 @@ const Docker = {
         ${githubToken ? '--env USE_EXIT_CODE=false' : '--env USE_EXIT_CODE=true'} \
         ${image} \
         --
-        /entrypoint.sh` ;
+        bash /entrypoint.sh` ;
 
     await exec(command, undefined, { silent });
   },
