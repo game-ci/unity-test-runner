698c08cf4e9bd80d79d87c933c2797a889bcb356
Michal Cichra <mikz@users.noreply.github.com>
2022-11-03 19:14:51 +0100

    feat: ensure cleanup of docker containers (#198)
    Cancelled or timeouted workflow would keep the docker container running.
    Closes game-ci/unity-test-runner#197
    
    This has two parts:
    
    Part one. The entrypoints.
    
    `runs.post`: GitHub Action metadata allow running something after the
    action (regardless of a failure, crash, timeout, ...).
    https://docs.github.com/en/actions/creating-actions/metadata-syntax-for-github-actions#runspost
    
    However, it needs to be a `.js` file and it can't be configured to pass
    any arguments.
    
    There already was `index.js` used as the main entrypoint.
    The build process of this file uses typescript compiler and ncc to pack
    all dependencies into one .js file. And ncc has no way of generating
    multiple files in one go, so the only solution would be to run ncc twice
    and generate two independent files.
    
    That would be quite unfortunate, wasting time and storage. So I rather
    came up with a new entrypoint that symlinked from two locations.
    And this new entrypoint understands how it was executed, so it can run
    the correct behaviour. This makes it easy to add `runs.pre` if needed.
    
    This new entrypoint is in `index.ts`. The original `index.ts` is now in
    `main.ts`.
    
    Part two.
    The signals. I've tried:
    * try/catch/finally around the `await Docker.run`. Catch and finally are
      not executed when process receives SIGINT. See the discussion in: https://github.com/nodejs/node/discussions/29480
    * New AbortController and AbortSignal. Great concept, but the
      action.exec does not support it. So it can't be aborted.
    * Doing cleanup on `process.on('exit')`. Unfortunately you can't really
      do async stuff from there, so can't really run the docker rm command
    to delete the container.
    * Using `process.on('SIGINT')`. For some reason that wasn't really
      executing for me. I'd not put my hand in fire for this, but I assume
    because it was in the signal handler it does something special, or would
    heed to be scheduled for later with `setTimeout(0)`.
    
    Evaluating all these I came to a conclusion that it is fragile and just
    relying on a `runs.post` is much better and safer.
    `

---
+++ src/main.ts
@@ -0,0 +1,58 @@
+import * as core from '@actions/core';
+import { Action, Docker, ImageTag, Input, Output, ResultsCheck } from './model';
+
+export async function run() {
+  try {
+    Action.checkCompatibility();
+
+    const { workspace, actionFolder } = Action;
+    const {
+      editorVersion,
+      customImage,
+      projectPath,
+      customParameters,
+      testMode,
+      coverageOptions,
+      artifactsPath,
+      useHostNetwork,
+      sshAgent,
+      gitPrivateToken,
+      githubToken,
+      checkName,
+      chownFilesTo,
+    } = Input.getFromUser();
+    const baseImage = new ImageTag({ editorVersion, customImage });
+    const runnerContext = Action.runnerContext();
+
+    try {
+      await Docker.run(baseImage, {
+        actionFolder,
+        editorVersion,
+        workspace,
+        projectPath,
+        customParameters,
+        testMode,
+        coverageOptions,
+        artifactsPath,
+        useHostNetwork,
+        sshAgent,
+        gitPrivateToken,
+        githubToken,
+        chownFilesTo,
+        ...runnerContext,
+      });
+    } finally {
+      await Output.setArtifactsPath(artifactsPath);
+      await Output.setCoveragePath('CodeCoverage');
+    }
+
+    if (githubToken) {
+      const failedTestCount = await ResultsCheck.createCheck(artifactsPath, githubToken, checkName);
+      if (failedTestCount >= 1) {
+        core.setFailed(`Test(s) Failed! Check '${checkName}' for details.`);
+      }
+    }
+  } catch (error: any) {
+    core.setFailed(error.message);
+  }
+}
--- src/model/docker.ts
+++ src/model/docker.ts
@@ -1,8 +1,33 @@
-import { existsSync, mkdirSync } from 'fs';
+import { existsSync, mkdirSync, readFileSync, rmSync } from 'fs';
+import type { RunnerContext } from './action';
 import { exec } from '@actions/exec';
 import path from 'path';
 
+/**
+ * Build a path for a docker --cidfile parameter. Docker will store the the created container.
+ * This path is stable for the whole execution of the action, so it can be executed with the same parameters
+ * multiple times and get the same result.
+ */
+const containerIdFilePath = parameters => {
+  const { runnerTemporaryPath, githubAction } = parameters;
+
+  return path.join(runnerTemporaryPath, `container_${githubAction}`);
+};
+
 const Docker = {
+  /**
+   *  Remove a possible leftover container created by `Docker.run`.
+   */
+  async ensureContainerRemoval(parameters: RunnerContext) {
+    const cidfile = containerIdFilePath(parameters);
+    if (!existsSync(cidfile)) {
+      return;
+    }
+    const container = readFileSync(cidfile, 'ascii').trim();
+    await exec(`docker`, ['rm', '--force', '--volumes', container], { silent: true });
+    rmSync(cidfile);
+  },
+
   async run(image, parameters, silent = false) {
     let runCommand = '';
     switch (process.platform) {
@@ -15,6 +40,7 @@ const Docker = {
       default:
         throw new Error(`Operation system, ${process.platform}, is not supported yet.`);
     }
+
     await exec(runCommand, undefined, { silent });
   },
 
@@ -40,12 +66,14 @@ const Docker = {
     if (!existsSync(githubHome)) mkdirSync(githubHome);
     const githubWorkflow = path.join(runnerTemporaryPath, '_github_workflow');
     if (!existsSync(githubWorkflow)) mkdirSync(githubWorkflow);
+    const cidfile = containerIdFilePath(parameters);
     const testPlatforms = (
       testMode === 'all' ? ['playmode', 'editmode', 'COMBINE_RESULTS'] : [testMode]
     ).join(';');
 
     return `docker run \
                 --workdir /github/workspace \
+                --cidfile "${cidfile}" \
                 --rm \
                 --env UNITY_LICENSE \
                 --env UNITY_LICENSE_FILE \
@@ -112,6 +140,7 @@ const Docker = {
 
     const githubHome = path.join(runnerTemporaryPath, '_github_home');
     if (!existsSync(githubHome)) mkdirSync(githubHome);
+    const cidfile = containerIdFilePath(parameters);
     const githubWorkflow = path.join(runnerTemporaryPath, '_github_workflow');
     if (!existsSync(githubWorkflow)) mkdirSync(githubWorkflow);
     const testPlatforms = (
@@ -120,6 +149,7 @@ const Docker = {
 
     return `docker run \
                 --workdir /github/workspace \
+                --cidfile "${cidfile}" \
                 --rm \
                 --env UNITY_LICENSE \
                 --env UNITY_LICENSE_FILE \
