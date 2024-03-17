056d560f5872646e6f77194b0465dae99de5de8d
Andrew Kahr <22359829+AndrewKahr@users.noreply.github.com>
2024-03-16 17:49:00 -0700

    Support new unity versioning scheme with regex matching groups

--- src/model/unity-version-parser.test.ts
+++ src/model/unity-version-parser.test.ts
@@ -11,6 +11,12 @@ describe('UnityVersionParser', () => {
       m_EditorVersionWithRevision: 2022.3.7f1 (b16b3b16c7a0)`;
       expect(UnityVersionParser.parse(projectVersionContents)).toBe('2022.3.7f1');
     });
+
+    it('parses Unity 6000 and newer from ProjectVersion.txt', () => {
+      const projectVersionContents = `m_EditorVersion: 6000.0.0f1
+      m_EditorVersionWithRevision: 6000.0.0f1 (cb45f9cae8b7)`;
+      expect(UnityVersionParser.parse(projectVersionContents)).toBe('6000.0.0f1');
+    });
   });
 
   describe('read', () => {
